09714215924bf55e43bdea80a7f675f6f1ec48a1
Krishna <sbhat97@gatech.edu>
2024-03-16 12:30:57 -0400

    Added auto pairs

--- init.lua
+++ init.lua
@@ -72,6 +72,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+  -- Auto  bracket pairs
+  'jiangmiao/auto-pairs',
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
