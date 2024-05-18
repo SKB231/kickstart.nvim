34218f0aac4979ed8956a3be4b3883feb00455bf
Krishna <sbhat97@gatech.edu>
2024-05-18 17:42:45 -0400

    Added new theme

--- init.lua
+++ init.lua
@@ -215,12 +215,15 @@ require('lazy').setup({
 
   {
     -- Theme inspired by Atom
-    -- previous: 'navarasu/onedark.nvim',
-    'savq/melange-nvim',
+    -- previous 2: 'navarasu/onedark.nvim',
+    -- previous:
+    -- 'savq/melange-nvim',
+    -- priority = 1000,
+    -- config = function()
+    --   vim.cmd.colorscheme 'melange'
+    -- end,
+    'catppuccin/nvim',
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'melange'
-    end,
   },
 
   {
@@ -230,7 +233,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'catppuccin',
         component_separators = '|',
         section_separators = '',
       },
@@ -641,6 +644,59 @@ require('which-key').register({
 require('mason').setup()
 require('mason-lspconfig').setup()
 
+
+-- Catppuccin theme setup:
+require("catppuccin").setup({
+  flavour = "auto", -- latte, frappe, macchiato, mocha
+  background = {    -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = false, -- disables setting the background color.
+  show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+  term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+  dim_inactive = {
+    enabled = false,              -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15,            -- percentage of the shade to apply to the inactive window
+  },
+  no_italic = false,              -- Force no italic
+  no_bold = false,                -- Force no bold
+  no_underline = false,           -- Force no underline
+  styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" },      -- Change the style of comments
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+    -- miscs = {}, -- Uncomment to turn off hard-coded styles
+  },
+  color_overrides = {},
+  custom_highlights = {},
+  default_integrations = true,
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    treesitter = true,
+    notify = false,
+    mini = {
+      enabled = true,
+      indentscope_color = "",
+    },
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
 
 
 
